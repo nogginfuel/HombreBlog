9294e5272638b5ed545494eb84139efee0af4a48
Jef Lippiatt <jeflippiatt@gmail.com>
2013-10-03 11:24:43 -0400

    Delete server.rb
    I do not intend to run on a Ruby build.

--- server.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'sinatra'
-
-
-set :public_folder, File.dirname(__FILE__) + '/website'
-
-get '/' do
-	          send_file File.join(settings.public_folder, 'index.html')
-end
